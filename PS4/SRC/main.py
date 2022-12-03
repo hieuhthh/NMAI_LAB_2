e831c04ac654f32b0ca1cbee7ae9842f4f1cfa07
hieuhthh <nguyenmautronghieu@gmail.com>
2022-12-03 20:46:20 +0700

    1231231

--- PS4/SRC/main.py
+++ PS4/SRC/main.py
@@ -10,6 +10,7 @@ make_dir(OUTPUT_DIR)
 for file in sorted(os.listdir(INPUT_DIR)):
     print(f'Solving {file}')
     input_path = os.path.join(INPUT_DIR, file)
-    output_path = os.path.join(OUTPUT_DIR, file)
+    name_outfile = 'output' + file[5:]
+    output_path = os.path.join(OUTPUT_DIR, name_outfile)
     solve_end2end(input_path, output_path, DEBUG)
     print(f'Finished {file}\n')
